43bd824eaeeff9d1010b33021c26145c48828198
John Farrell <jsfarrell12@gmail.com>
2023-03-22 14:56:04 -0400

    functionality

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -24,123 +24,84 @@ import {
   FormLabel,
   ModalFooter,
   Tooltip,
+  useToast,
 } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { FiPlus, FiTrash } from "react-icons/fi";
 import BreadcrumbHandler from "../BreadcrumbHandler/BreadcrumbHandler";
-
-interface User {
-  name: string;
-  email: string;
-  role: "administrator" | "editor" | "viewer";
-}
-
-const users: User[] = [
-  {
-    name: "Alice Adams",
-    email: "alice@example.com",
-    role: "administrator",
-  },
-  {
-    name: "Bob Brown",
-    email: "bob@example.com",
-    role: "editor",
-  },
-  {
-    name: "Charlie Clark",
-    email: "charlie@example.com",
-    role: "viewer",
-  },
-  {
-    name: "David Davis",
-    email: "david@example.com",
-    role: "administrator",
-  },
-  {
-    name: "Emily Evans",
-    email: "emily@example.com",
-    role: "editor",
-  },
-  {
-    name: "Frank Fisher",
-    email: "frank@example.com",
-    role: "viewer",
-  },
-  {
-    name: "Grace Green",
-    email: "grace@example.com",
-    role: "administrator",
-  },
-  {
-    name: "Henry Hill",
-    email: "henry@example.com",
-    role: "editor",
-  },
-  {
-    name: "Isabel Ingram",
-    email: "isabel@example.com",
-    role: "viewer",
-  },
-  {
-    name: "Jack Jackson",
-    email: "jack@example.com",
-    role: "administrator",
-  },
-  {
-    name: "Kate King",
-    email: "kate@example.com",
-    role: "editor",
-  },
-  {
-    name: "Liam Lee",
-    email: "liam@example.com",
-    role: "viewer",
-  },
-  {
-    name: "Mia Mitchell",
-    email: "mia@example.com",
-    role: "administrator",
-  },
-  {
-    name: "Nate Nelson",
-    email: "nate@example.com",
-    role: "editor",
-  },
-  {
-    name: "Olivia Owens",
-    email: "olivia@example.com",
-    role: "viewer",
-  },
-  {
-    name: "Peter Parker",
-    email: "peter@example.com",
-    role: "administrator",
-  },
-  {
-    name: "Quinn Quinn",
-    email: "quinn@example.com",
-    role: "editor",
-  },
-  {
-    name: "Ryan Roberts",
-    email: "ryan@example.com",
-    role: "viewer",
-  },
-  {
-    name: "Sarah Scott",
-    email: "sarah@example.com",
-    role: "administrator",
-  },
-  {
-    name: "Tom Thompson",
-    email: "tom@example.com",
-    role: "editor",
-  },
-];
+import axios from "axios";
 
 const roleOptions = ["administrator", "editor", "viewer"];
 
 function AddUser() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+  const [role, setRole] = useState(null);
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+
+  const isDisabled = role == null || name == null || email == null;
+
+  const handleNameChange = (e: any) => {
+    let value = e.target.value;
+    if (value === "") {
+      setName(null);
+    } else setName(value);
+  };
+  const handleEmailChange = (e: any) => {
+    let value = e.target.value;
+    if (
+      value === "" ||
+      !value.includes("@") ||
+      !value.includes(".") ||
+      value.length < 5
+    ) {
+      setEmail(null);
+    } else setEmail(value);
+  };
+  const handleRoleChange = (e: any) => {
+    let value = e.target.value;
+    if (value === "") {
+      setRole(null);
+    } else setRole(value);
+  };
+
+  const handleAddClick = () => {
+    setIsLoading(true);
+    axios
+      .post("http://localhost:3000/api/v1/users", {
+        role,
+        name,
+        email,
+      })
+      .then((res) => {
+        setIsLoading(false);
+        toast({
+          title: "User added.",
+          description: "User successfully added!",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+          position: "top",
+        });
+        window.location.reload();
+        onClose();
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+        toast({
+          title: "Error adding user.",
+          description: "There was an error adding the user.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+          position: "top",
+        });
+        onClose();
+      });
+  };
 
   const userData = {
     name: "Dwight Schrute",
@@ -163,17 +124,20 @@ function AddUser() {
           <ModalBody pb={6}>
             <FormControl>
               <FormLabel>Name</FormLabel>
-              <Input placeholder={userData.name} />
+              <Input placeholder={userData.name} onChange={handleNameChange} />
             </FormControl>
 
             <FormControl mt={4}>
               <FormLabel>Email</FormLabel>
-              <Input placeholder={userData.email} />
+              <Input
+                placeholder={userData.email}
+                onChange={handleEmailChange}
+              />
             </FormControl>
 
             <FormControl mt={4}>
               <FormLabel>Role</FormLabel>
-              <Select placeholder={`   `}>
+              <Select placeholder={`   `} onChange={handleRoleChange}>
                 {roleOptions.map((role) => (
                   <option key={role} value={role}>
                     {role}
@@ -184,7 +148,13 @@ function AddUser() {
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="green" mr={3}>
+            <Button
+              colorScheme="green"
+              mr={3}
+              onClick={handleAddClick}
+              isLoading={isLoading}
+              isDisabled={isDisabled}
+            >
               Add
             </Button>
             <Button onClick={onClose}>Cancel</Button>
@@ -195,8 +165,46 @@ function AddUser() {
   );
 }
 
-function RemoveUser({ userData }: { userData: User }) {
+function RemoveUser(userData: any, setUsersVersion: any) {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const toast = useToast();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleRemoveClick = () => {
+    setIsLoading(true);
+    axios
+      .delete("http://localhost:3000/api/v1/users", {
+        data: {
+          id: userData.userData._id.$oid,
+        },
+      })
+      .then((res) => {
+        toast({
+          title: "User removed.",
+          description: "User successfully removed!",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+          position: "top",
+        });
+        window.location.reload();
+        setIsLoading(false);
+        onClose();
+      })
+      .catch((err) => {
+        toast({
+          title: "Error removing user.",
+          description: "There was an error removing the user.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+          position: "top",
+        });
+        setIsLoading(false);
+      });
+  };
 
   return (
     <>
@@ -217,7 +225,12 @@ function RemoveUser({ userData }: { userData: User }) {
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="red" mr={3}>
+            <Button
+              colorScheme="red"
+              mr={3}
+              onClick={handleRemoveClick}
+              isLoading={isLoading}
+            >
               Remove
             </Button>
             <Button onClick={onClose}>Cancel</Button>
@@ -228,7 +241,109 @@ function RemoveUser({ userData }: { userData: User }) {
   );
 }
 
+function UserRow(userData: any) {
+  let { key, name, email, role, _id } = userData.userRow;
+
+  const toast = useToast();
+
+  const uploadChanges = (isRole: boolean, e: any) => {
+    let data = isRole
+      ? {
+          id: _id.$oid,
+          email: email,
+          role: e.target.value,
+        }
+      : {
+          id: _id.$oid,
+          email: e.target.value,
+          role: role,
+        };
+    axios
+      .put("http://localhost:3000/api/v1/users", data)
+      .then((res) => {
+        toast({
+          title: "User updated.",
+          description: "User successfully updated!",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+          position: "top",
+        });
+      })
+      .catch((err) => {
+        toast({
+          title: "Error updating user.",
+          description: "There was an error updating the user.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+          position: "top",
+        });
+      });
+  };
+
+  return (
+    <Tr key={key}>
+      <Td>{name}</Td>
+      <Td>
+        <InputGroup size="sm">
+          <Input
+            placeholder="Email"
+            defaultValue={email}
+            disabled={role === "administrator"}
+            variant="filled"
+            onBlur={(e) => {
+              uploadChanges(false, e);
+            }}
+          />
+        </InputGroup>
+      </Td>
+      <Td>
+        <Select
+          defaultValue={role}
+          size="sm"
+          disabled={role === "administrator"}
+          variant="filled"
+          onChange={(e) => {
+            uploadChanges(true, e);
+          }}
+        >
+          {roleOptions.map((role) => (
+            <option key={role} value={role}>
+              {role}
+            </option>
+          ))}
+        </Select>
+      </Td>
+      <Td isNumeric>
+        <RemoveUser userData={{ _id, name, email, role }} />
+      </Td>
+    </Tr>
+  );
+}
+
 export default function Users() {
+  const [users, setUsers] = useState<[]>([]);
+  const toast = useToast();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/v1/users")
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        toast({
+          title: "Error fetching users.",
+          description: "There was an error fetching the users.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+          position: "top",
+        });
+      });
+  }, [toast]);
+
   return (
     <Flex minH={"100vh"} bg={"gray.100"} pb={10} pr={10}>
       <Box w={"full"}>
@@ -252,38 +367,12 @@ export default function Users() {
               </Tr>
             </Thead>
             <Tbody>
-              {users.map(({ name, email, role }) => {
+              {users.map(({ _id, name, email, role }, key) => {
                 return (
-                  <Tr>
-                    <Td>{name}</Td>
-                    <Td>
-                      <InputGroup size="sm">
-                        <Input
-                          placeholder="Email"
-                          defaultValue={email}
-                          disabled={role === "administrator"}
-                          variant="filled"
-                        />
-                      </InputGroup>
-                    </Td>
-                    <Td>
-                      <Select
-                        defaultValue={role}
-                        size="sm"
-                        disabled={role === "administrator"}
-                        variant="filled"
-                      >
-                        {roleOptions.map((role) => (
-                          <option key={role} value={role}>
-                            {role}
-                          </option>
-                        ))}
-                      </Select>
-                    </Td>
-                    <Td isNumeric>
-                      <RemoveUser userData={{ name, email, role }} />
-                    </Td>
-                  </Tr>
+                  <UserRow
+                    key={key}
+                    userRow={{ name, email, role, key, _id }}
+                  />
                 );
               })}
             </Tbody>
